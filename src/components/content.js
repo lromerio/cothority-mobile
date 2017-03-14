2657d1d5bbc576428aed168a0c820f5dd28feade
Lucio <lucio.romerio@epfl.ch>
2017-03-14 19:25:41 +0100

    Set up pop-organizer skeleton: no functionalities yet

--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import Tappable from 'react-tappable';
+import React from 'react'
+import Tappable from 'react-tappable'
 
-import './content.css';
+import './content.css'
 
 import ServersStatus from './servers-status/servers-status';
+import PopOrganizer from './pop/pop-organizer'
+import PopAttendee from './pop/pop-attendee'
+
 
 export default class Content extends React.Component{
 
@@ -12,18 +15,30 @@ export default class Content extends React.Component{
 
         this.state = {
             currentPage:  0
-        };
+        }
     }
 
     updatePage(index) {
-        this.setState({currentPage: index});
+        this.setState({currentPage: index})
     }
 
+    //TODO: substitute <br/> with css?!?
     renderMenu(){
         return (
             <div className="menu">
+                <br/>
                 <Tappable className="menu-item" onTap={() => this.updatePage(1)} >
                     Status
+                </Tappable>
+                <br/>
+                <br/>
+                <Tappable className="menu-item" onTap={() => this.updatePage(2)} >
+                    PoP Organizer
+                </Tappable>
+                <br/>
+                <br/>
+                <Tappable className="menu-item" onTap={() => this.updatePage(3)} >
+                    PoP Attendee
                 </Tappable>
             </div>
         )
@@ -39,6 +54,15 @@ export default class Content extends React.Component{
             case 1:
                 content = <ServersStatus/>;
                 break;
+            case 2:
+                content = <PopOrganizer/>;
+                break;
+            case 3:
+                content = <PopAttendee/>;
+                break;
+            default:
+                content = this.renderMenu();
+                break;
         }
 
         return (
@@ -51,6 +75,6 @@ export default class Content extends React.Component{
 
                 {content}
             </div>
-        );
+        )
     }
 }
---
+++ src/components/pop/pop-attendee.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+//TODO
+export default class PopAttendee extends React.Component {
+    render() {
+         return (
+             <div>
+                 <h2> Attend a PoP party</h2>
+                 <p> Attendee part goes here</p>
+             </div>
+         )
+    }
+}
