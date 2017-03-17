1d180e17d82bcd9975b1675eb07c50b4b1018f68
Lucio <lucio.romerio@epfl.ch>
2017-03-18 00:56:47 +0100

    Implement first step functionality

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -2,56 +2,63 @@ import CothorityMessages from '../lib/cothority-messages'
 
 const EMPTY_MESSAGE = new Uint8Array([]);
 
-/**
- * Source: https://github.com/Gilthoniel/cothority-web
- */
 class CothorityWebsocket {
 
-  status = {};
+    status = {};
 
-  getStatus(address) {
+    getStatus(address) {
 
-    // The promise will resolve when the socket sends back the response of the call
-    return new Promise((resolve, reject) => {
-      if (!this.status.hasOwnProperty(address) || this.status[address].readyState > 2) {
-        const socket = new WebSocket(`ws://${address}/Status/Request`);
-        socket.binaryType = 'arraybuffer';
-
-        this.status[address] = socket;
-      }
-
-      const socket = this.status[address];
-
-      function onError(e) {
-        reject(e);
-        socket.removeEventListener('error', onError);
-      }
-
-      socket.addEventListener('error', onError);
-
-      function onMessage(e) {
-        const s = CothorityMessages.decodeStatusResponse(e.data);
-        resolve(s);
-
-        // Clean
-        socket.removeEventListener('message', onMessage);
-      }
-
-      socket.addEventListener('message', onMessage);
-
-      if (socket.readyState === 0) {
-        // socket is not yet opened
-        socket.addEventListener('open', () => {
-          // Send an empty message to trigger the status
-          socket.send(EMPTY_MESSAGE);
+        // The promise will resolve when the socket sends back the response of the call
+        return new Promise((resolve, reject) => {
+            this.status[address] = createSocket(
+                this.status[address],
+                address + '/Status/Request',
+                (e) => reject(e),
+                (data) => resolve(CothorityMessages.decodeStatusResponse(data)),
+                EMPTY_MESSAGE
+            );
         });
-      } else {
-        socket.send(EMPTY_MESSAGE);
-      }
-    });
 
-  }
-
+    }
 }
 
 export default new CothorityWebsocket();
+
+/**
+ * Use the existing socket or create a new one if required
+ * @param socket WebSocket old socket
+ * @param address String ws address
+ * @param error Function callback if an error occurred
+ * @param callback Function callback when a message is received
+ * @param message ArrayBuffer the message to send
+ * @returns {*}
+ */
+function createSocket(socket, address, error, callback, message) {
+    if (!socket || socket.readyState > 2) {
+        socket = new WebSocket(`ws://${address}`);
+        socket.binaryType = 'arraybuffer';
+    }
+
+    function onError(e) {
+        socket.removeEventListener('error', onError);
+        error(e);
+    }
+    socket.addEventListener('error', onError);
+
+    function onMessage(e) {
+        socket.removeEventListener('message', onMessage);
+        callback(e.data);
+    }
+    socket.addEventListener('message', onMessage);
+
+    if (socket.readyState === 0) {
+        socket.addEventListener('open',() => {
+            socket.send(message);
+        });
+    }
+    else {
+        socket.send(message);
+    }
+
+    return socket;
+}
