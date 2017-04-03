7feff911447c28819e7a552206028efb5a5a0fc1
Lucio <lucio.romerio@epfl.ch>
2017-04-03 19:31:43 +0200

    Add websocket basic tests

--- test/websocket.specs.js
+++ test/websocket.specs.js
@@ -1,3 +1,71 @@
+
+/*
+MOCK_STATUS_RESPONSE = {
+    server: {
+        address: faker.internet.ip(),
+        description: faker.lorem.sentence(),
+        id: new Uint8Array([0, 0, 0, 0]),
+        public: new Uint8Array([1, 1, 1, 1])
+    },
+    system: {
+        Status: {
+            field: {
+                test: faker.lorem.sentence()
+            }
+        }
+    }
+};
+
+describe('CothorityProtobuf', function() {
+
+    //var messageReceived = false;
+    var test = {
+        messageReceived: function () {
+        }
+    };
+
+
+    beforeEach(function(done) {
+        var mockServer = new Server('ws://localhost/Status/Request');
+        mockServer.on('message', test.messageReceived);
+
+
+        var mockErrorServer = new Server('ws://localhost:1/Status/Request');
+        mockErrorServer.on('message', function () {
+            //messageReceived = true;
+        });
+
+        var fixture = '<div id="ws_result">test</div>' +
+            '<input id="address" value="localhost">' +
+            '<input id="pin" value="test">';
+
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            fixture);
+
+        spyOn(test, 'messageReceived');
+
+        getStatus();
+
+        setTimeout(function () {
+            done();
+        }, 5000);
+        //while(!messageReceived) {
+            // Wait response
+        //}
+    });
+
+    it('should create and persist the socket', function () {
+
+
+        expect(test.messageReceived).toHaveBeenCalled();
+
+    });
+
+});
+
+*/
+
 describe('websocket', function() {
 
     // inject the HTML fixture for the tests
@@ -13,7 +81,7 @@ describe('websocket', function() {
 
     describe('fail', function() {
         it('should print error message', function() {
-            fail('');
+            callbacks.fail('');
 
             var expectedHtml = '<span style="color: red;">ERROR: </span>undefined';
 
@@ -23,7 +91,7 @@ describe('websocket', function() {
 
     describe('success', function() {
         it('should print success message', function() {
-            success('');
+            callbacks.success('');
 
             var expectedHtml = '<span style="color: green;">SUCCESS: </span>undefined';
 
