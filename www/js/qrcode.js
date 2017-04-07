9e5b5ad2353d87dc2265733226538251d3720ae7
lromerio <lucio.romerio@epfl.ch>
2017-04-08 00:24:48 +0200

    Adapt database to cisc; increase coverage

--- www/js/qrcode.js
+++ www/js/qrcode.js
@@ -1,8 +1,15 @@
+/**
+ * Scan a qr-code using the official phonegap plugin.
+ * In case of success returns the scanned string, otherwise an empty string
+ * and alert the user.
+ *
+ * @returns {string}
+ */
 function qrcodeScan() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
             if(!result.cancelled) {
-                alert("Barcode type is: " + result.format + "\n" + "Decoded text is: " + result.text);
+                return result.text;
             } else {
                 alert("You have cancelled scan");
             }
@@ -11,15 +18,19 @@ function qrcodeScan() {
             alert("Scanning failed: " + error);
         }
     );
+
+    return '';
 }
 
+/**
+ * Encode the given string in to a qr-code and shows it to the user.
+ * In case of error alert the user.
+ *
+ * @param toEncode
+ */
 function qrcodeCreate(toEncode) {
     cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, toEncode,
-        function (success) {
-            alert("Encode succes: " + success);
-        },
-        function (fail) {
-            alert("Encoding failed: " + fail);
-        }
+        function (success) {alert("Encode succes: " + success)},
+        function (fail) {alert("Encoding failed: " + fail)}
     );
 }
